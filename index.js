3d12485fd7fe8baf85e0d8cdbb21c550e9ff3d61
Ankit <ankit0panchal@gmail.com>
2022-12-27 21:05:03 +0530

    CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const cors = require('cors');
 require('dotenv').config();
 const express = require("express");
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./server/schema/schema')
 const connectDB = require('./server/Config/db')
+
 
 const port = process.env.PORT || 3000;
 
@@ -10,6 +12,7 @@ const app = express();
 
 // Connect to DB
 connectDB();
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
